54bc6cb22632a1e0f464bde14e2658662282193e
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-13 18:44:13 -0500

    Refreshing Core Syntax
    12

--- first-app.js
+++ first-app.js
@@ -1,4 +1,13 @@
-const fs = require('fs'); //import file system
+//global variables
+var name = 'Max'
+var age = 29
+var hasHobbies = true;
 
-fs.writeFileSync('hello.txt', 'Hello') //write path to file, then file contents.
+console.log(name);
 
+function summarizeUser(userName, userAge, userHasHobbies){
+    return 'Name is ' + userName + ", age is " + userAge + 
+    ", user has hobbies: " + userHasHobbies; 
+}
+
+console.log(summarizeUser(name,age,hasHobbies));
