3ead6a1e17fe77f1b0c1860a202a28447c8ece36
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-14 18:52:12 -0500

    61 How Middleware Works

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const http = require('http'); //looks for a local file named http that comes with node
-//will import routes.js and look for module.exports and see what is registered in there
-const routes = require('./route');
+const http = require('http');
+//3rd party packages
+const express = require('express');
+//creates an express application.
+const app = express();//this is a valid request handler
 
-console.log(routes.someText);
+//we travel from middleware to middleware from top to bottom by calling next().
 
-const server = http.createServer(routes.handler);
+//this allows us to add another middleware function.
+app.use((req, res, next) => { //next is a function that will be passed to the anon function, and the anon function is passed to the use function. weird.
+    //we want to travel on to the next middleware
+    console.log('in the middleware');
+    //we need to type next(); here to allow the code to get to the next parts of code
+    //if we never send this, we will be stuck here
+    next();//allows the request to continue to the next middleware in line.
+    //WE SHOULD SEND NEXT IF WE DO NOT SEND A RESPONSE OTHERWISE IT WILL JUST DIE
+
+});
+
+app.use((req, res, next) => { //next is a function that will be passed to the anon function, and the anon function is passed to the use function. weird.
+    //we want to travel on to the next middleware
+    console.log('in the 2nd middleware');
+    res.send('<h1>hello from express!</h1>');//allows us to send a response, and attach a body which is of type 'any'
+
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
