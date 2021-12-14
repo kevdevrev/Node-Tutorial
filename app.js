034dbdf384e2c1bded9b757316ba9706b2d12f0a
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-14 14:23:11 -0500

    32 Routing Requests

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http'); //looks for a local file named http that comes wit
 
 //creates the server (inside is nextgen js)
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>hi</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type ="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        //we want to quit before the other res writes / headers occur after the res end.
+        return res.end();
+    }
     //sending a header back to the browser, basically the content type is text/html
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
