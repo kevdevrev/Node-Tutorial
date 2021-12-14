c5d490bb0d4ad554b1f1cf341112114826662312
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-13 19:27:46 -0500

    Objects
    15

--- first-app.js
+++ first-app.js
@@ -1,26 +1,13 @@
-//global variables
-const name = 'Max' //var is kind of outdated. use let or const instead.
-let age = 29
-const hasHobbies = true;
-
-age = 30;
-
-console.log(name);
-
-//arrow function, annoymous 
-//its shorter but we use it because:
-//
-const summarizeUser = (userName, userAge, userHasHobbies) => {
-    return 'Name is ' + userName + ", age is " + userAge + 
-    ", user has hobbies: " + userHasHobbies; 
+//object creation
+//has key value pairs
+const person = {
+    name: 'Max',
+    age: 29,
+    //how to add a function to an object
+    greet() {
+        console.log('Hi, I am ' + this.name 
+        + ' and my age is: ' + this.age);
+    }
 }
 
-//shorter way of doing it
-const add = (a,b) => a + b;
-//if you have one argument only, you can have just the argument without parenthesis
-//const addOne = a => a + 1;
-const addRandom = () => 1 + 2;
-console.log(addRandom());
-
-console.log(summarizeUser(name,age,hasHobbies));
-console.log(add(1,2));
+person.greet();
