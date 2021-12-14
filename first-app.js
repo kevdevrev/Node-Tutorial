f82504cf26c234264c811b821a511f87fd393ba2
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-14 02:17:49 -0500

    Async promises, callbacks and more

--- first-app.js
+++ first-app.js
@@ -1,32 +1,55 @@
-//object creation
-//has key value pairs
-const person = {
-    name: 'Max',
-    age: 29,
-    //how to add a function to an object
-    greet() {
-        console.log('Hi, I am ' + this.name 
-        + ' and my age is: ' + this.age);
-    }
-}
+//our argument will eventually call in our inner function once the timer is done.
+// const fetchData = callback => {
 
-// const printName = (personData) => {
-//     console.log(personData.name);
-// }
+//we no longer use any callback here, and instead do resolve.
+const fetchData = () => {
+    //we make our promise here.
 
-//Object Destrucoring. { }
-//it will pull name out of the object
-const printName = ({ name }) => {
-    console.log(name);
-}
+    //new is our constructor obviously
+    //promise constructor is built into js
+    //resolve means it does the promise succesfully, reject means it fails and throws an error.
+    //normally this is behind the scenes hidden away, and not written by me.
+    const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        //callback('Done!');
+        //we now do resolve instead
+        resolve('Done');
+    }, 1500);
+    });
+    //this is synchronus, it will return before the code inside is run.
+    return promise;
+};
 
-printName(person);
 
-//we can make constants off the value of the person lol
-const { name, age} = person;
+//TIMED DELAYS
+//ASYNCHRONUS
+//Its a "Callback" function that executes in the future and JS recognizes it.
+//If we keep nesting callbacks, theres something called "promises" which are in Node.js.
+//3rd party packages use promises for us usually. We are gonna use a handmade one.
+setTimeout(() => {
+    console.log('Timer is done!');
+    //we are calling fetchData here
+    // fetchData(text => {
+    //     console.log(text);
+    // });
+    //now we no longer pass a callback , but instead just do:
+    fetchData()
+    .then(text => {
+        console.log(text);
+        //we return another promise here to let the next then run.
+        return fetchData();
+    })
+    .then(text2 => {
+        console.log(text2);
+        //no promise returned here, so we are done nesting
+    })// we can just have a series of .then instead of nesting so many and its more clear now
+    ; // this is callable on a promise, and returns a promise
+}, 2000);
 
-console.log(name, age);
+//SYNCHRONUS
+console.log('hello!');
+console.log('hi');
 
-const hobbies = ['Sports', 'Cooking'];
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1,hobby2);
+
+
+
