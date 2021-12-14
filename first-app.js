6a35cbc87df830412854f12ffce8e704353fa020
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-13 19:23:32 -0500

    anon functions

--- first-app.js
+++ first-app.js
@@ -7,9 +7,20 @@ age = 30;
 
 console.log(name);
 
-function summarizeUser(userName, userAge, userHasHobbies){
+//arrow function, annoymous 
+//its shorter but we use it because:
+//
+const summarizeUser = (userName, userAge, userHasHobbies) => {
     return 'Name is ' + userName + ", age is " + userAge + 
     ", user has hobbies: " + userHasHobbies; 
 }
 
+//shorter way of doing it
+const add = (a,b) => a + b;
+//if you have one argument only, you can have just the argument without parenthesis
+//const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+console.log(addRandom());
+
 console.log(summarizeUser(name,age,hasHobbies));
+console.log(add(1,2));
