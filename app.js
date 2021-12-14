f114fc93f4bdf7976bbfd15017b6d4af817b76bb
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-14 16:00:09 -0500

    35 Understanding Event Driven Execution

--- app.js
+++ app.js
@@ -21,20 +21,27 @@ const server = http.createServer((req, res) => {
             console.log(chunk);//chunk we cant work with
             body.push(chunk);
         });//allows us to listen to certain events, we want to listen for the data event and the fucntion to be executed for every incoming request
-        req.on('end', () => { //We can now know that all the chunks have been read once we are here.
+        //if we add return to req.on, the code below won't run
+        return req.on('end', () => { //We can now know that all the chunks have been read once we are here.
             //we now need to buffer all the chunks to be able to work with them.
             const parsedBody = Buffer.concat(body).toString();//will concat all the body to the Buffer, we convert it to a string for our purpose here
             console.log(parsedBody);//this we can work with.
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message); //this depends on the data, so it has to be here.
+            //we moved it here cause we want it to occur with this listener
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         })
         //below actually occurs before the above, because node does not wait and pause, it registers stuff
         //to-be-executed action.
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+        // res.statusCode = 302;
+        // res.setHeader('Location', '/');
+        // return res.end();
     }
+    //this won't occur if we return res.on('end...) at the same tiem, but will occur before it, unless we return that res.on(end)
     //sending a header back to the browser, basically the content type is text/html
+    //this WILL run before our earlier req.on('end, ...)
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>hi</title></head>');
@@ -43,5 +50,9 @@ const server = http.createServer((req, res) => {
     res.end();
 
 }); 
+
+//Notes:
+//Sending a response does not mean our event listners give up. They will still execute even if the response is already gone.
+//So if you need certain code to run before the respond is returned, put it in the event listener.
 
 server.listen(3000);
