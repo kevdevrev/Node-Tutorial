36a7975ba097ae3ea60aa5e958f959d4645d1bec
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-14 14:18:39 -0500

    31

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http'); //looks for a local file named http that comes wit
 
 //creates the server (inside is nextgen js)
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    //sending a header back to the browser, basically the content type is text/html
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>hi</title></head>');
+    res.write('</html>');
+    //we are done writing, send it to the browser
+    res.end();
+
 }); 
 
 server.listen(3000);
