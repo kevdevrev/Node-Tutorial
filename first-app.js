fae70ee9ea3bdae557472c91415ef6cf09c5ce33
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-13 20:11:38 -0500

    Destructuring Objects and Arrays
    19

--- first-app.js
+++ first-app.js
@@ -10,32 +10,23 @@ const person = {
     }
 }
 
-//also works on objects SPREAD OPERATOR
-const copiedPerson = {...person};
-console.log(copiedPerson);
+// const printName = (personData) => {
+//     console.log(personData.name);
+// }
+
+//Object Destrucoring. { }
+//it will pull name out of the object
+const printName = ({ name }) => {
+    console.log(name);
+}
+
+printName(person);
+
+//we can make constants off the value of the person lol
+const { name, age} = person;
+
+console.log(name, age);
 
 const hobbies = ['Sports', 'Cooking'];
-
-
-//slice copies an array
-// const copiedArray = hobbies.slice();
-//this will give an array with another array in it. Its not a copy.
-//its an array where the first element is the other array. Nested array.
-// const copiedArray = [hobbies];
-
-//SPREAD OPERATOR
-//it pulls out all the elements of the array or porperties of an object
-//and puts it to whereever is around that spread operator.
-const copiedArray = [...hobbies];
-console.log(copiedArray);
-
-//not flexible. Lets use the rest operator instead.
-// const toArray = (arg1,arg2,arg3) => {
-//     return[arg1, arg2, arg3];
-// };
-//REST OPERATOR
-const toArray = (...args) => {
-    return args;
-};
-
-console.log(toArray(1,2,3,4));
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1,hobby2);
