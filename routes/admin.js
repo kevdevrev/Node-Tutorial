f0adbae19620ecbea99e8839f88b1b4ab2847658
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-15 18:02:05 -0500

    70 Serving HTML pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 //router can use post, get, use etc.
 //we replace app with router here.
-router.get('add-product',(req, res, next) => { 
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');//allows us to send a response, and attach a body which is of type 'any'
+router.get('/add-product',(req, res, next) => { 
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 
 });
 
 //we changed it to post, so that it only takes post requests.
-router.post('product', (req,res,next) => {
+router.post('/product', (req,res,next) => {
     console.log(req.body);
     //go back to '/' route after logic done
     res.redirect('/');
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
 
 //get and post use an exact match for the path
 router.get('/',(req, res, next) => { //next is a function that will be passed to the anon function, and the anon function is passed to the use function. weird.
-    res.send('<h1>hello from express!</h1>');
+    //__dirname is a variable which holds the absolute path on the os of this project folder
+    //it gives us the path of where we use it
+    //../ makes it go up one level in the directory.
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 
 
 });
