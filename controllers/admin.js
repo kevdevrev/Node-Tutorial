8cdfbe59ca95fd6d0da8870576ea3268872a6011
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-12-27 13:04:12 -0500

    111 Displaying Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,6 @@ exports.getAddProductPage =  (req, res, next) => {
       const imageUrl = req.body.imageUrl;
       const price = req.body.price;
       const description = req.body.description;
-
       //create a new object based off imported class
       const product = new Product(title, imageUrl, description, price);
       product.save(); // now we save the product in our array.
